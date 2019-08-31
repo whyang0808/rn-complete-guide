23bf97d172621e5a55e91d5ade049ccc89dd6470
Yang Wei Hao <whyang0808@gmail.com>
2019-09-01 00:13:51 +0800

    Styling List Items

--- App.js
+++ App.js
@@ -25,7 +25,11 @@ export default function App() {
         <Button title="ADD" onPress={addGoalHandler}/>
       </View>
       <View>
-        {courseGoals.map(goal => <Text key={goal}>{goal}</Text>)}
+        {courseGoals.map(goal => (
+          <View key={goal} style={styles.listItems}>
+            <Text >{goal}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -45,5 +49,12 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
+  },
+  listItems: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ccc',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
